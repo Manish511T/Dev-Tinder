d37a4ca935b6aec7006ddc622abd9173d9c146ec
Manish <mkthakur2301@gmail.com>
2026-03-10 00:43:57 +0530

    setup: server

---
+++ Backend/src/app.js
@@ -0,0 +1,19 @@
+import express, { json } from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/health', (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
