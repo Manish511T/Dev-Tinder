2dd2418e7883b846ea26c2e9cc1940d4eef75ef1
Manish <mkthakur2301@gmail.com>
2026-03-18 00:24:03 +0530

    connect MongoDB

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,38 +1,46 @@
-import express, { json } from "express"
-import dotenv from "dotenv"
+import express, { json } from "express";
+import dotenv from "dotenv";
 import { adminAuth, userAuth } from "./middlewares/auth.js";
+import  connectDB  from "./config/db.js";
+
 dotenv.config();
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json())
+app.use(express.json());
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
+});
+
 
 app.use("/admin", adminAuth);
 
-app.use("/user/login", (req, res)=>{
+app.use("/user/login", (req, res) => {
   res.send("You're are logged in");
-})
-app.get("/user/data", userAuth,(req, res)=>{
-  res.send("User Data Sent");
-})
-
-app.get("/admin/getAllData", (req,res)=>{
-
-  res.send("Get All Data");
-
 });
- 
-app.get("/admin/deleteUser", (req,res)=>{
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("Get All Data");
+});
+
+
+
+app.get("/admin/deleteUser", (req, res) => {
   res.send("Deleted a user");
 });
 
-app.get('/health', (req, res) => {
+app.get("/health", (req, res) => {
   res.send("Server running");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
