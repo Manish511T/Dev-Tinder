7255de6687cdab8fe854265d6e50f959e064ae90
Manish <mkthakur2301@gmail.com>
2026-03-19 00:37:41 +0530

    get user by email

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,7 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import User from "./models/user.js"
+import User from "./models/user.js";
 dotenv.config();
 connectDB();
 
@@ -12,18 +12,61 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, emailId, password } = req.body;
+    const existingUser = await User.find({ emailId });
 
-  console.log(req.body);
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-  // try {
-  //   await user.save();
+    const user = await User.create({
+      firstName,
+      lastName,
+      emailId,
+      password,
+    });
 
-  //   res.status(201).json({ message: "user registered" })
-  // } catch (error) {
-  //   res.status(400).send(error.message)
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailId: user.emailId,
+      },
+    });
 
-  // }
-})
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    const user = await User.find({ emailId: userEmail });
+
+    if (!user ) {
+      res.status(404).json({ message: "User not found" })
+    }
+    res.send(user);
+  } catch (error) {
+    res.status.json({ message: "Something went wrong!" });
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.send(users);
+
+  } catch (error) {
+    res.status.json({ message: "Something went wrong!" });
+  }
+});
+
 
 
 app.get("/health", (req, res) => {
